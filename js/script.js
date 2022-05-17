d25b6a96b7c40d26d92d8d756d44f04fd58c8c06
dindamaylan <dindamaylan@gmail.com>
2022-05-17 11:36:32 +0700

    update accordion

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-document.getElementById("hapusAja")
+// document.getElementById("hapusAja")
+const iconMin = document.getElementById("iconMin")
+const iconPlus = document.getElementById("iconPlus")
+const cardBody = document.getElementById("cardBody")
+
+function displayIcon() {
+    iconMin.classList.remove("displayNone")
+    iconPlus.classList.add("displayNone")
+    cardBody.classList.remove("displayNone")
+}
+
+function displayIconPlus() {
+    iconMin.classList.add("displayNone")
+    iconPlus.classList.remove("displayNone")
+    cardBody.classList.add("displayNone")
+}
